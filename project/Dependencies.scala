7ea1a0246dac69fa61ea5ea89c4bd0b7266de44f
Artem Tartakynov <artem.tad@gmail.com>
2017-09-19 17:58:21 +0700

    updated play-ws to 1.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
 
   object Libraries {
     val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
-    val playWS: ModuleID = "com.typesafe.play" %% "play-ahc-ws-standalone" % "1.0.4"
+    val playWS: ModuleID = "com.typesafe.play" %% "play-ahc-ws-standalone" % "1.1.1"
     val specs2: Seq[ModuleID] = Seq(
       "specs2-core",
       "specs2-junit",
