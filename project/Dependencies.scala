4c35e20e0e22d9dd98e3193ecfd83b37f4ff294a
Artem Tartakynov <artem.tad@gmail.com>
2018-09-23 20:10:23 +0700

    updated dependencies and move to sbt 1.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val scala = "2.11.11"
-    val crossScala = List(scala, "2.12.2")
+    val scala = "2.11.12"
+    val crossScala = List(scala, "2.12.6")
   }
 
   object Libraries {
-    val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
-    val playWS: ModuleID = "com.typesafe.play" %% "play-ahc-ws-standalone" % "1.1.1"
+    val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+    val playWS: ModuleID = "com.typesafe.play" %% "play-ahc-ws-standalone" % "1.1.10"
     val specs2: Seq[ModuleID] = Seq(
       "specs2-core",
       "specs2-junit",
