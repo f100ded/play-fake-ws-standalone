cd56363c1a186677c5a2e480939eb5c04106e81b
Artem Egorkine <artem.egorkine@adeona.com>
2023-09-28 01:04:05 +0300

    Update dependencies & scala versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val scala = "3.2.1"
-    val crossScala = List(scala, "2.13.7")
+    val scala = "3.3.1"
+    val crossScala = List(scala, "2.13.12")
   }
 
   object Libraries {
-    val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
-    val playWS: ModuleID = "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.0-M2"
+    val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
+    val playWS: ModuleID = "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.2"
     val specs2: Seq[ModuleID] = Seq(
       "specs2-core",
       "specs2-junit",
-    ).map("org.specs2" %% _ % "4.19.0")
+    ).map("org.specs2" %% _ % "4.20.2")
   }
 }
